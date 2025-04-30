b3459d318f66911d5c325fd78a742f58b9a6ab94
wataruw <saruchichi@gmail.com>
2025-04-30 16:09:29 +0900

    薙刀式V16にした。

---
+++ keyboards/crkbd/qmk/qmk_firmware/keymaps/naginata_v16/rules.mk
@@ -0,0 +1,13 @@
+VIA_ENABLE         = yes
+ENCODER_MAP_ENABLE = yes
+
+# Add naginata support
+UNICODE_ENABLE = yes
+SRC += naginata_v16.c
+SRC +=  nglist.c
+SRC +=  nglistarray.c
+
+# Enable extended layout
+LAYOUT_SPLIT_3X6_3_EX2 = yes
+
+MOUSEKEY_ENABLE = yes
